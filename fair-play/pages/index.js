5043d38e99587d605d2ccd55d90e393c06ff61d5
Weverton <wevertonbezzera013@gmail.com>
2022-12-11 17:17:33 -0300

    [Update] added navbar

---
+++ fair-play/pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+    return (
+        <div className="flex justify-center">
+            <h1>Hello</h1>
+        </div>
+    )
+}
---
+++ fair-play/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import Footer from "../Footer/Footer"
+import Navbar from "../Navbar/Navbar"
+
+
+export default function Layout() {
+    return (
+        <>
+            <Navbar/>
+            <Footer/>
+        </>
+    )
+}
