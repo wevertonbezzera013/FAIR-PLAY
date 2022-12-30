763159c6cb562b1a3d4de4e49a470148fc7ddc80
Weverton <wevertonbezzera013@gmail.com>
2022-12-30 10:15:46 -0300

    sanity

--- front-end/blog-app-try/sanity.config.ts
+++ front-end/blog-app-try/sanity.config.ts
@@ -3,12 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
 export default defineConfig({
   name: 'default',
   title: 'blog-app-try',
 
-  projectId: '8xuaa6d3',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
