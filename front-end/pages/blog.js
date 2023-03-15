969e42d89216a023c8878e64e40d810eaeee44ee
Weverton <wevertonbezzera013@gmail.com>
2023-03-15 19:50:42 -0300

    integration with graphql
    I have fully integrated with Graphql.

---
+++ front-end/pages/blog.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { GraphQLClient, gql } from 'graphql-request'
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+const graphcms = new GraphQLClient("https://api-sa-east-1.hygraph.com/v2/clfa5s4ft1p1d01t9gum6d7mp/master")
+
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      datePublished
+      slug
+      content {
+        html
+      }
+      author {
+        name
+        avatar {
+          url
+        }
+      }
+      coverPhoto {
+        url
+      }
+    }
+  }
+`
+
+export async function getStaticProps() {
+  const { posts } = await graphcms.request(QUERY)
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  }
+}
+
+const Blog = ({ posts }) => {
+  return (
+    <div>
+
+      <Header />
+
+      <div className="bg-img-about text-center flex">
+        <div className="container absolute top-48">
+          <span className="text-white text-5xl font-bold text-left break-words">
+            BLOG
+          </span>
+        </div>
+      </div>
+
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>Author: {post.author.name}</p>
+            <img src={post.author.avatar.url} alt={`${post.author.name}'s avatar`} />
+            <div dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+            <img src={post.coverPhoto.url} alt='Cover photo' />
+          </div>
+        ))}
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Blog
